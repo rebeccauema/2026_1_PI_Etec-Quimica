5bc6af353d88ff9d264c2510dfb5e428503ce639
Isabela Nakai <bebelanakai@gmail.com>
2026-05-14 20:18:28 -0300

    fix: nbactations

---
+++ src/main/java/br/com/pi_2026_1_etec/service/JogoService.java
@@ -0,0 +1,11 @@
+package br.com.pi_2026_1_etec.service;
+
+import java.util.Scanner;
+
+public class JogoService {
+    Scanner scanner = new Scanner(System.in);
+    public void iniciar () {
+        br.com.pi_2026_1_etec.service.JogoService p1 = new br.com.pi_2026_1_etec.service.JogoService();
+        p1.enunciado = "Qual o nome desse material";
+    }
+}
