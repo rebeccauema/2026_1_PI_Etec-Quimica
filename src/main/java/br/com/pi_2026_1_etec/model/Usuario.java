1b5ec103fb9f559828da58a1defd178631818250
miguelrdias1 <miguel.r.dias1@gmail.com>
2026-05-20 22:16:28 -0300

    resolve merge conflict

--- src/main/java/br/com/pi_2026_1_etec/model/Usuario.java
+++ src/main/java/br/com/pi_2026_1_etec/model/Usuario.java
@@ -15,5 +15,6 @@ public class Usuario {
     public void setEmail(String email) { this.email = email; }
     public String getSenha() { return senha; }
     public void setSenha(String senha) { this.senha = senha; }
+
 }
 
--- src/main/java/br/com/pi_2026_1_etec/repository/UsuarioRepository.java
+++ src/main/java/br/com/pi_2026_1_etec/repository/UsuarioRepository.java
@@ -1,8 +1,12 @@
 package br.com.pi_2026_1_etec.repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 import br.com.pi_2026_1_etec.model.Usuario;
 import br.com.pi_2026_1_etec.util.ConexaoBD;
-import java.sql.*;
 
 public class UsuarioRepository {
 
@@ -17,7 +21,7 @@ public class UsuarioRepository {
             ps.setString(3, usuario.getSenha());
             ps.executeUpdate();
         }
-    }
+    }   //Insere um novo usuário no banco de dados e verifica se o email já está cadastrado
 
     public boolean emailJaExiste(String email) throws SQLException {
         String sql = "SELECT id FROM usuario WHERE email = ?";
@@ -29,5 +33,23 @@ public class UsuarioRepository {
             ResultSet rs = ps.executeQuery();
             return rs.next(); 
         }
+    }   
+    public Usuario buscarPorEmail(String email) throws SQLException {
+    String sql = "SELECT id, nome, email, senha FROM usuario WHERE email = ?";//prepara o comando pro sql
+     try (Connection con = ConexaoBD.getConexao();
+         PreparedStatement ps = con.prepareStatement(sql)) {
+             ps.setString(1, email);//troca o placeholder pelo email recebido
+        ResultSet rs = ps.executeQuery();
+        if (rs.next()) {
+            Usuario u = new Usuario();
+            u.setId(rs.getInt("id"));
+            u.setNome(rs.getString("nome"));
+            u.setEmail(rs.getString("email"));
+            u.setSenha(rs.getString("senha"));
+            return u;
+             } //retorna o usuario com os atributos puxados do sql
+        return null;//se não ter usuario no BD, retorna nulo.
+            }
+        }
     }
-}
+
